c6d07973c477fa391376872c5ca8f556479ecd57
Timur <tsivakov@yandex.ru>
2019-07-18 20:36:10 +0300

    Добавил модуль card.js

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,23 @@
 'use strict';
-var map = document.querySelector('.map');
-var MAP_FADED_CLASS = 'map--faded';
-/**
- * Функция удаляет у элемента класс
- * @param {Element} className
- * @param {string} classForRemove
- * @return {void} возвращает элемент без класса
- */
-var setupFunction = function (className, classForRemove) {
-  return className.classList.remove(classForRemove);
-};
+(function () {
+  /**
+   * Функция удаляет у элемента класс
+   * @param {Element} className
+   * @param {string} classForRemove
+   * @return {void} возвращает элемент без класса
+   */
+  var setupFunction = function (className, classForRemove) {
+    return className.classList.remove(classForRemove);
+  };
+  /**
+   * Функция возвращает проивольное целое число из диапазона
+   * @param {number} min
+   * @param {number} max
+   * @return {number} возвращает целое число
+   */
+  var generateRandomInteger = function (min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
+  };
+})();
