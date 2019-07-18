de7ce93017d7a3bd5cda9e90daccbf802f9c5403
Timur <tsivakov@yandex.ru>
2019-07-18 20:37:11 +0300

    Добавил функцию в модуль card.js

--- js/card.js
+++ js/card.js
@@ -1,4 +1,11 @@
 'use strict';
 (function () {
-
+  /**
+   * Функция возвращает рандомный элемент из массива характеристик объявления
+   * @param {string[]} adsParameters
+   * @return {string} возвращает элемент массива характеристик объявления
+   */
+  var getRandomAdsParameter = function (adsParameters) {
+    return adsParameters[Math.floor(Math.random() * adsParameters.length)];
+  };
 })();
